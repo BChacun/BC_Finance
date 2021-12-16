b1d26143cbc38e34dd4cc08aa729cedca6db6ea5
Baptiste Chacun <baptiste.chacun@imt-atlantique.net>
2021-12-16 17:12:52 +0100

    SMA 2

--- apps/trading_apps/SMA.py
+++ apps/trading_apps/SMA.py
@@ -11,13 +11,43 @@ import numpy as np
 import yfinance as yf
 
 
+import plotly.express as px
+
 # get relative data folder
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("../datasets").resolve()
 
+pd.options.plotting.backend = "plotly"
+
+data = yf.Ticker("NVDA").history(period='5y')
 
 
-df = yf.Ticker("NVDA").history(period='5y')
+# SMA :
+
+
+df = data["Open"].to_frame()
+df.rename(columns={'Open': 'price',  }, inplace=True)
+
+# SMA hyperparameter :
+
+sma_s = 50
+sma_l = 200
+
+df = df / df.iloc[0]
+
+df["SMA_S"] = df.price.rolling(sma_s).mean()
+df["SMA_L"] = df.price.rolling(sma_l).mean()
+
+
+df["position"] = np.where(df["SMA_S"] > df["SMA_L"], 1, -1)
+
+
+df.rename(columns={'price': 'open_price',  }, inplace=True)
+fig = df.plot(
+    title="SMA Model", template="simple_white",
+              labels=dict(index="Date", value="price variation", variable="legend")
+    )
+
 
 
 
@@ -30,6 +60,8 @@ layout = html.Div([
     page_size=10,
 
     ),
+     
+    dcc.Graph(figure=fig),
      
   ])
 
